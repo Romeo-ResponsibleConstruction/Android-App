f935f37cbed3a7b6c96de9e80a8c68fed5a251d9
Connor P <32711911+ConnorP501@users.noreply.github.com>
2023-02-28 12:28:36 +0000

    Revert "working on camera-cropping"
    This reverts commit 8f9685c9bbdfeac52924db2f2c7ecc29a254f3ee.

--- app/src/main/java/com/example/rc_app/storage/InternalRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.example.rc_app.storage
-
-interface InternalRepository<T> {
-    fun saveToInternalStorage(filetype: T): String
-    fun getFromInternalStorage(filepath: String): T
-    fun deleteFromInternalStorage(filepath: String): Boolean
-}
